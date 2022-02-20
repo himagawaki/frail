fd9311f7be8544a93a2dbd5849d0ca378549664f
hiroaki imagawa <94121911+himagawaki@users.noreply.github.com>
2022-02-20 18:48:37 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -10,3 +10,41 @@
       //次の質問にfitをつけて出力。
     })
   });
+
+//選択ボタンデータを配列に入れてカウントする関数
+var countA;
+//data-value="a"を選択した数を入れる変数
+var countB;
+//data-value="b"を選択した数を入れる変数
+var box =[];
+//それぞれのデータを配列に入れるための変数box
+$(".btn").each(function(){
+  $(this).on('click',function(){
+    var value = $(this).data("value");
+    box.push(value);
+    //data-value値をboxに入れる。(配列に値を入れるときはpush()を使用)
+
+    countA = box.filter(function(x){
+      return x === "a"
+    }).length;
+    //aの数を変数countAへ入れる
+    
+    countB = box.filter(function(y){
+      return y === "b"
+    }).length;
+    //bの数を変数countBへ入れる
+  });
+});
+
+//結果を出力する関数
+$('.end').on('click',function(){
+  //endクラスをクリックしたときの関数
+  
+  if( countA > countB ) {
+    $('#answer_01').css("display",""); //回答1
+    //answer_01のdisplayを""へ
+  } else {
+    $('#answer_02').css("display","");//回答2
+    //answer_02のdisplayを""へ
+  }
+});
