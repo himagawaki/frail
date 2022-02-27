1aa5e206c0f83fc789ce281c32d337fc5e833e4f
hiroaki imagawa <94121911+himagawaki@users.noreply.github.com>
2022-02-27 13:03:52 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
     $(".btn").on("click", function () {
       //btnクラスをクリック後の関数処理
       $(this).closest("div").css("display", "none");
-      //質問画面にあたらる親要素divをdisplay:none;にする
+      //質問画面にあたる親要素divをdisplay:none;にする
       id = $(this).attr("href");
       //次の質問hrefをidに格納
       $(id).addClass("fit").fadeIn("slow").show();
